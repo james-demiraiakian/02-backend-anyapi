e6979e5afa22dc27490bd92dcf66bed78b192a86
James Demiraiakian <james.armen@gmail.com>
2022-03-15 16:07:16 -0700

    findAll() test written, failing, as expected

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/Cat');
 
 describe('01-backend-anyapi routes', () => {
   beforeEach(() => {
@@ -21,5 +22,12 @@ describe('01-backend-anyapi routes', () => {
     const res = await request(app).post('/api/v1/cats').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(Number), ...expected });
+  });
+
+  test('fetch a list of cats', async () => {
+    const expected = await Cat.findAll();
+    const res = await request(app.get('api/v1/cats'));
+
+    expect(res.body).toEqual(expected);
   });
 });
